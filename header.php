3e6fb89740fd43fe33f696ce3f7f101c843d4cd9
m <creative@gmail.com>
2025-09-27 21:21:39 -0700

    feat: change

--- header.php
+++ header.php
@@ -14,12 +14,12 @@
     <header class="w-full flex justify-between items-center">
     <a href="<?php echo home_url(); ?>">
       <section class="header-logo flex justify-center items-center">
-        
+        <p></p>
       </section>
     </a>
     <section class="header-nav w-full flex justify-end items-center" style="padding-right:20px">
       
-      <a style="background-color:#737373; color:white; padding:10px; border-radius:8px"  href="<?php echo home_url(); ?>/textbook">Textbook</a>
+      <a style="background-color:#737373; color:white; padding:10px; border-radius:8px"  href="<?php echo home_url(); ?>/textbook">Tedfsdsdffxtbook</a>
       <a style="background-color:#737373; color:white; padding:10px; border-radius:8px"  href="<?php echo home_url(); ?>/reference">Reference</a>
       <a style="background-color:#737373; color:white; padding:10px; border-radius:8px"  href="<?php echo home_url(); ?>/contact">Contact</a>
     </section>
